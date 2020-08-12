9169a82da663fef0099cfd0cca593fceb527c9b1
Alex Dymanis <alex.dymanis@bedbath.com>
2020-08-12 13:08:27 -0400

    added some more methods

--- main.py
+++ main.py
@@ -17,9 +17,27 @@ class Alpaca:
         self.api = tradeapi.REST(key, secret, base_url=url) # or use ENV Vars shown below
         
     def account(self):   
-        print(self.api.get_account())
+        return(self.api.get_account())
+
+    def lastquote(self,symbol):   
+        return(self.api.get_last_quote(symbol))
+
+    def history(self,symbol,timeframe,limit):   
+        return(self.api.get_barset(symbol,timeframe,limit))
+
+    def averageflux(self,symbol,timeframe,limit):
+        #get % of flux, high/low based on timeframe and snapshot period
+        #ie: show average daily fluctuation in price over 5 days. or do hourly fluctionation over 2 days. 
+        return('nothing')
+
 
 #Logic Starts here
 connect = Alpaca()
 
-connect.account()
+#connect.account()
+
+#connect.quote('VXRT')
+
+barset = connect.history('VXRT','day','5')
+
+print(barset['VXRT'][0])
