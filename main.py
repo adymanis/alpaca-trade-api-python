3a3d1d72c2a3fc05985d393ec4430c0e5510ecb6
Alex Dymanis <alex.dymanis@bedbath.com>
2020-08-12 12:23:59 -0400

    initial connection

---
+++ main.py
@@ -0,0 +1,24 @@
+import alpaca_trade_api as tradeapi
+import json
+
+
+class Alpaca:   
+    # init method or constructor    
+    def __init__(self):   
+
+        #Read Config file
+        with open('config.json') as json_file:
+            config = json.load(json_file)
+
+            key = config['Account']['API-Key']
+            secret = config['Account']['Secret']
+
+        self.api = tradeapi.REST(key, secret, base_url='https://paper-api.alpaca.markets') # or use ENV Vars shown below
+        
+    def account(self):   
+        print(self.api.get_account())
+
+#Logic Starts here
+connect = Alpaca()
+
+connect.account()
